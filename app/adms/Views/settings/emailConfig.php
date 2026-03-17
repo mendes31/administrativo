f31af4a9a7e5e67a38bcf7079830e41f16657b7e
mendes31 Rafael Mendes <raffaell_mendez@hotmail.com>
2026-03-17 10:49:19 -0300

    Correções form config e-mail e envio de teste

--- app/adms/Views/settings/emailConfig.php
+++ app/adms/Views/settings/emailConfig.php
@@ -9,6 +9,7 @@ $config = $this->data['email_config'] ?? [];
                     <h4 class="mb-0">Configuração de Servidor de E-mail (SMTP)</h4>
                 </div>
                 <div class="card-body">
+                    <?php include './app/adms/Views/partials/alerts.php'; ?>
                     <!-- Dicas de configuração -->
                     <div class="alert alert-info">
                         <h6><i class="fas fa-info-circle"></i> Dicas para Gmail:</h6>
@@ -121,7 +122,7 @@ function testarEmail() {
         // Criar um formulário temporário para enviar o POST
         const form = document.createElement('form');
         form.method = 'POST';
-        form.action = '<?= $_ENV['URL_ADM'] ?>test-email-config';
+        form.action = '<?= $_ENV['URL_ADM'] ?>email-config/test';
         // Adicionar ao DOM e enviar
         document.body.appendChild(form);
         form.submit();
