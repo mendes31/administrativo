2e1c025356b6c006ac86343e9747c9eb038a651b
mendes31 Rafael Mendes <raffaell_mendez@hotmail.com>
2026-03-25 13:25:26 -0300

    Teste Login1 branch teste

--- app/adms/Views/login/login.php
+++ app/adms/Views/login/login.php
@@ -14,7 +14,7 @@ use App\adms\Helpers\CSRFHelper;
         </div>
 
         <div class="card-header">
-            <h3 class="text-center font-weight-light my-4">Login</h3>
+            <h3 class="text-center font-weight-light my-4">Login1</h3>
         </div>
 
         <div class="card-body">
